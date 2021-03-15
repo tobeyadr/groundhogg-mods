3642e2ac13945543d7918e2e954c8ea2f4a0982b
tobeyadr <adrian.methoss@gmail.com>
2021-03-15 17:10:25 -0400

    Initial commit

---
+++ groundhogg-mods.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: Groundhogg Modifications
+Plugin URI: http://yoursite.com
+Description: Custom code snippets for Groundhogg.
+Version: 1.0
+Author: Your Name
+Author URI: http://yoursite.com
+*/
+
+// Add custom code snippets here
+// If the snippet you are copying has "<?php" at the beginning, delete it
+// Always deactivate the plugin before making changes.
